dd75f7dd46797686804892b6e4e369bef2d6707b
Maria <zeldacamila0913@gmail.com>
2022-11-29 13:54:20 -0500

    changed whatsapp number

--- src/components/ButtonWhatsapp/ButtonWhatsapp.jsx
+++ src/components/ButtonWhatsapp/ButtonWhatsapp.jsx
@@ -6,10 +6,10 @@ const ButtonWhatsapp = () => {
 
   return (
     <button className='WAButton-container' >
-      <a className='logoWApc-container' href='https://api.whatsapp.com/send?phone=573195250514&text=¡Hola!%20Quiero%20comprar%20un%20producto%20de%20la%20CASA%20%F0%9F%8F%A1%F0%9F%9B%B8%F0%9F%91%BD'>
+      <a className='logoWApc-container' href='https://api.whatsapp.com/send?phone=573022711169&text=¡Hola!%20Quiero%20comprar%20un%20producto%20de%20la%20CASA%20%F0%9F%8F%A1%F0%9F%9B%B8%F0%9F%91%BD'>
         <img id='logoWA' src={logoWA} alt='' />
       </a>
-      <a className='logoWAmobile-container' href='https://api.whatsapp.com/send?phone=573195250514&text=¡Hola!%20Quiero%20comprar%20un%20producto%20de%20la%20CASA%20%F0%9F%8F%A1%F0%9F%9B%B8%F0%9F%91%BD'>
+      <a className='logoWAmobile-container' href='https://api.whatsapp.com/send?phone=573022711169&text=¡Hola!%20Quiero%20comprar%20un%20producto%20de%20la%20CASA%20%F0%9F%8F%A1%F0%9F%9B%B8%F0%9F%91%BD'>
         <img id='logoWA' src={logoWAmobile} alt='' />
       </a>
     </button>
