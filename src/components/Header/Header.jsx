12dc97c89d55c3a7e0c98c95981c1f5d1d0bd2b9
Maria <zeldacamila0913@gmail.com>
2024-02-02 17:54:48 -0500

    edited header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,28 @@
 import logo from '../../images/LOGOlacasa5a.png'
 import logo2 from '../../images/LOGOlacasa5b.png'
-import cartImg from '../../images/cartLogo.png'
-import { useNavigate } from 'react-router-dom'
+import logoMin from '../../images/LOGOTIPO.png'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const Header = () => {
 
   const navigate = useNavigate()
+  const location = useLocation();
 
   const handleClickLogo = () => {
     navigate('/')
   }
-  const handleClickCart = () => {
-    navigate('/cart')
-  }
-   
+  
+  // Determinar qué logo mostrar basado en la ruta
+  const isHomePage = location.pathname === '/';
+  const logoToShow = isHomePage ? logo : logoMin;
   return (
-      <header className="header">
+    <header className={`header ${!isHomePage ? 'header-not-home' : ''}`}>
         <div className='empty'></div>
         <div className='logo-container'>
-          <img className='header_logo' src={logo} alt ="logo" onClick={handleClickLogo}/>
-          <img className='header_logo2' src={logo2} alt ="logo2"/>
+        <img className={`header_logo ${!isHomePage ? 'header_logo-not-home' : ''}`} src={logoToShow} alt="logo" onClick={() => navigate('/')} />
+        <img className='header_logo2' src={logo2} alt ="logo2" onClick={handleClickLogo}/>
         </div>
         <div className='cartlogo-container'>
-          
         </div>
       </header>
   )
